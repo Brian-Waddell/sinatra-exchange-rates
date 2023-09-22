7043a90ab208c83c7d4454b4ac16b54020431158
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-22 19:35:28 +0000

    commit

--- app.rb
+++ app.rb
@@ -3,16 +3,41 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+def dictionary 
+raw_data = HTTP.get("https://api.exchangerate.host/symbols")
+
+ parse_data = JSON.parse(raw_data)
+
+ hop = parse_data.fetch("symbols").keys
+ 
+end
 get("/") do
- raw_data = HTTP.get("https://api.exchangerate.host/symbols")
+ 
+  @var1 = dictionary
 
- @parse_data = JSON.parse(raw_data)
-
- @hop = @parse_data.fetch("symbols").keys
- erb(:new)
+ 
+ erb(:Home)
 end
 
-get("/:currency") do 
-@new_var = params.fetch("currency")
+get("/:currency") do
+  
+  @ans = params.fetch("currency")
+
+  @var1 = dictionary 
+ 
   erb(:currency)
 end 
+
+get("/:currency/:input") do 
+  @ans = params.fetch("currency")
+
+  @answer = params.fetch("input")
+
+ raw = HTTP.get("https://api.exchangerate.host/convert?from=#{@ans}&to=#{@answer}") 
+
+ pro = JSON.parse(raw)
+
+ @solution = pro.fetch("result")
+  
+  erb(:second)
+end
